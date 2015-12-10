7ed82f4b82592344f7eb95088325b36e7ac88a29
antonxuanquang <antonxuanquang@yahoo.com>
2015-12-10 14:04:14 -0600

    moving generator class into generator package

--- src/Model/Generator/DataTableGenerator.java
+++ src/Model/Generator/DataTableGenerator.java
@@ -1,4 +1,4 @@
-package Model;
+package Model.Generator;
 
 import java.io.BufferedReader;
 import java.io.File;
--- src/Model/Generator/HashTableGenerator.java
+++ src/Model/Generator/HashTableGenerator.java
@@ -1,4 +1,4 @@
-package Model;
+package Model.Generator;
 
 import java.util.ArrayList;
 
