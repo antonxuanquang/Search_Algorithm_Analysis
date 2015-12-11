11fc4b86075508903d4cb1522de6a0853191b477
antonxuanquang <antonxuanquang@yahoo.com>
2015-12-10 18:43:38 -0600

    adding getters and setters for Model

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -3,9 +3,71 @@ package Model;
 import java.util.ArrayList;
 
 public class Model {
-	int [] hashTable;
-	ArrayList<Integer> rawData, missingKeys, presentKeys;
+	private int [] hashTable;
+	private ArrayList<Integer> randomKeys, missingKeys, presentKeys;
+	private int tableSize;
+	private String collisionHandler, hashingFunction;
+	private Counter count = new Counter();
 	
 	public Model() {}
 	
+	public int[] getHashTable() {
+		return hashTable;
+	}
+	
+	public void setHashTable (int [] table) {
+		hashTable = table;
+	}
+	
+	public ArrayList<Integer> getRandomKeysTable() {
+		return randomKeys;
+	}
+	
+	public void setRandomKeysTable(ArrayList<Integer> table) {
+		randomKeys = table;
+	}
+	
+	public ArrayList<Integer> getMissingKeysTable() {
+		return missingKeys;
+	}
+	
+	public void setMissingKeysTable(ArrayList<Integer> table) {
+		missingKeys = table;
+	}
+	
+	public ArrayList<Integer> getPresentKeysTable() {
+		return presentKeys;
+	}
+	
+	public void setPresentKeysTable(ArrayList<Integer> table) {
+		presentKeys = table;
+	}
+	
+	public String getCollisionHandler () {
+		return collisionHandler;
+	}
+	
+	public void setCollisionHandler(String handler) {
+		collisionHandler = handler;
+	}
+	
+	public String getHashingFunction() {
+		return hashingFunction;
+	}
+	
+	public void setHashingFunction(String function) {
+		hashingFunction = function;
+	}
+	
+	public int getTableSize() {
+		return tableSize;
+	}
+	
+	public void setTableSize(int size) {
+		tableSize = size;
+	}
+	
+	public Counter getCounterObject() {
+		return count;
+	}
 }
